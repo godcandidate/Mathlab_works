517cd417a48a9817d46fee4ac087e9d8fc1f3e40
godcandidate <102991532+godcandidate@users.noreply.github.com>
2024-06-23 23:15:50 +0000

    Create continous_signal.m

---
+++ Generating_sinusoidal_signals/method_2/continous_signal.m
@@ -0,0 +1,18 @@
+% Define parameters
+fs = 10; 
+N = 2.5;  
+L = 60/fs;  
+
+% Generate time vector (normalized frequency)
+k = 0:1/fs:L-1/fs;  
+k = k/L;  
+
+% Generate the sinusoidal signal
+s = sin(2*pi*N*k);
+
+% Plot the signal using plot (continuous line)
+plot(k, s);
+xlabel('Normalized Frequency (k)');
+ylabel('Amplitude');
+title('Sinusoidal Signal (Plot)');
+grid on;
