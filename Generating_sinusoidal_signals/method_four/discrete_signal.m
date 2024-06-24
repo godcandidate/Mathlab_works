2855f442582ec85b1705671db9a423d436196bb2
godcandidate <102991532+godcandidate@users.noreply.github.com>
2024-06-24 08:15:21 +0000

    Create discrete_signal.m

---
+++ Generating_sinusoidal_signals/method_four/discrete_signal.m
@@ -0,0 +1,20 @@
+% Define parameters
+fs = 10; 
+n = 60;   
+
+% Calculate frequency based on desired periods (n) and sampling rate
+f = 2.5 * fs / n;  
+
+% Generate time vector (seconds)
+k = 0:n-1;
+k = k * (1/fs); 
+
+% Generate the sinusoidal signal
+s = sin(2*pi*f*k);
+
+% Plot the signal using stem (discrete points)
+stem(k, s);
+xlabel('Time (seconds)');
+ylabel('Amplitude');
+title('Sinusoidal Signal (Stem)');
+grid on;
