a07b5c2519336013cc625032d9a5804aa8a57045
godcandidate <102991532+godcandidate@users.noreply.github.com>
2024-06-24 07:53:16 +0000

    Create discrete_signal.m

---
+++ Generating_sinusoidal_signals/method_three/discrete_signal.m
@@ -0,0 +1,20 @@
+% Define parameters
+fs = 10;
+N = 2.5; 
+
+% Calculate actual frequency based on desired periods and sampling rate
+% f = (number of periods) * (sampling frequency) / total duration (assumed 60 seconds)
+f = N * fs / 60; 
+
+% Generate time vector (normalized frequency) based on actual frequency
+k = 0:1/fs:(N/f)-1/fs; 
+
+% Generate the sinusoidal signal
+s = sin(2*pi*f*k);
+
+% Plot the signal using stem (discrete points)
+stem(s);
+xlabel('Normalized Frequency (k)');
+ylabel('Amplitude');
+title('Sinusoidal Signal (Stem)');
+grid on;
